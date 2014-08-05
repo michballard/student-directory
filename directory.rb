d1e54df7cb29ade6162d62f7f4023b7fb195dcfb
michballard <michballard@gmail.com>
2014-08-05 15:41:09 +0100

    Added input students method

--- directory.rb
+++ directory.rb
@@ -1,31 +1,3 @@
-#let's put all students into an array
-students = [
-	{:name => "Ruth Earle", :cohort => :august},
-	{:name => "Andy Gates", :cohort => :august}, 
-	{:name => "Marc Singh", :cohort => :august},
-	{:name => "Faisal Aydarus", :cohort => :august},
-	{:name => "Ethel Ng", :cohort => :august},
-	{:name => "Kevin Daniells", :cohort => :august},
-	{:name => "Maya Driver", :cohort => :august},
-	{:name => "Leopold Kwok", :cohort => :august},
-	{:name => "James McNeil", :cohort => :august},
-	{:name => "Jerome Pratt", :cohort => :august},
-	{:name => "David Wickes", :cohort => :august},
-	{:name => "Javier Silverio", :cohort => :august},
-	{:name => "Elliot Lewis", :cohort => :august},
-	{:name => "Ben Tillett", :cohort => :august},
-	{:name => "Vincent Koch", :cohort => :august},
-	{:name => "Michelle Ballard", :cohort => :august},
-	{:name => "Nick Roberts", :cohort => :august},
-	{:name => "Tatiana Soukiassian", :cohort => :august},
-	{:name => "Merve Silk", :cohort => :august},
-	{:name => "Albert Vallverdu", :cohort => :august},
-	{:name => "Lovis Schulze", :cohort => :august},
-	{:name => "Chris Oatley", :cohort => :august},
-	{:name => "Spike Lindsey", :cohort => :august},
-	{:name => "Henry Stanley", :cohort => :august}
-]
-
 def print_header
   puts "The students of my cohort at Makers Academy"
   puts "----------------"
@@ -40,7 +12,27 @@ end
 def print_footer(names)
   puts "Overall, we have #{names.length} great students"
 end
-#nothing happens until we call the methods
+
+def input_students
+	puts "Please enter the names of the students"
+	puts "To finish, just hit return twice"
+	# create an empty array
+	students = []
+	# get the first name
+	name = gets.chomp
+	# while the name is not empty, repeat this code
+	while !name.empty? do
+		# add the student hash to the array
+		students << {:name => name, :cohort => :august}
+		puts "Now we have #{students.length} students"
+		# get another name from the user
+		name = gets.chomp
+	end
+	# return the array of students
+	students
+end 
+
+students = input_students
 print_header
 print(students)
 print_footer(students)
