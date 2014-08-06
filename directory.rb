3c058a5bb094aed6751c0e48a0c5dc94acf271d9
michballard <michballard@gmail.com>
2014-08-06 12:21:38 +0100

    Replaced each method with until

--- directory.rb
+++ directory.rb
@@ -2,16 +2,6 @@ def print_header
   print "The students of my cohort at Makers Academy\n----------------\n"
 end
 
-def print_student(students)
-  students.each.with_index(1) do |student, index|
-    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
-  end
-end
-
-def print_footer(names)
-  puts "Overall, we have #{names.length} great students"
-end
-
 def input_students
 	print "Please enter the names of the students\n"
 	print "To finish, just hit return twice\n"
@@ -31,33 +21,65 @@ def input_students
 	students
 end 
 
-students = input_students
-print_header
-print_student(students)
-print_footer(students)
+# def print_student(students)
+#   students.each.with_index(1) do |student, index|
+#     puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+#   end
+# end
+
+# Rewriting above code using "while" control flow method
+# def print_student(studentlist)
+# 	count = 1
+# 	while count <= studentlist.length 
+# 		studentlist.select do |student|
+# 			puts "#{count}. #{student[:name]} (#{student[:cohort]} cohort)"
+# 			count += 1
+# 		end
+# 	end
+# end
+
+# Rewriting above code using "until" control flow method
+def print_student(studentlist)
+	count = 1
+	until count >= studentlist.length 
+		studentlist.select do |student|
+			puts "#{count}. #{student[:name]} (#{student[:cohort]} cohort)"
+			count += 1
+		end
+	end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.length} great students"
+end
 
 # List only students whose name begins with a specified letter
 def student_search(studentlist)
 	puts "What is the starting letter you would like to search for?"
 	# get the search character from user
 	character = gets.chomp.capitalize
+	puts "Names starting with #{character}:"
 	# perform search for character
 	studentlist.select do |student| 
 		puts "#{student[:name]}" if student[:name].start_with?(character)
 	end
 end
 
-student_search(students)
-
 # List only students with names shorter than specified number of characters
 def students_namelength(studentlist)
 	puts "What is the required maximum number of characters in full name?"
 	# get number of characters from user
 	namelength = gets.chomp.to_i
+	puts "Names with up to #{namelength} characters:"
 	# perform search for name lengths
 	studentlist.select do |student|
 		puts "#{student[:name]}" if student[:name].length <= namelength
 	end
 end	
 
+students = input_students
+print_header
+print_student(students)
+print_footer(students)
+student_search(students)
 students_namelength(students)
