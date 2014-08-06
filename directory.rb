8c20f9e9ca611d2af66a0a4c7521376ea0a2252a
michballard <michballard@gmail.com>
2014-08-06 10:55:51 +0100

    Updated character search

--- directory.rb
+++ directory.rb
@@ -1,11 +1,10 @@
 def print_header
-  puts "The students of my cohort at Makers Academy"
-  puts "----------------"
+  print "The students of my cohort at Makers Academy\n----------------\n"
 end
 
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def print_student(students)
+  students.each.with_index(1) do |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -14,8 +13,8 @@ def print_footer(names)
 end
 
 def input_students
-	puts "Please enter the names of the students"
-	puts "To finish, just hit return twice"
+	print "Please enter the names of the students\n"
+	print "To finish, just hit return twice\n"
 	# create an empty array
 	students = []
 	# get the first name
@@ -23,8 +22,8 @@ def input_students
 	# while the name is not empty, repeat this code
 	while !name.empty? do
 		# add the student hash to the array
-		students << {:name => name, :cohort => :august}
-		puts "Now we have #{students.length} students"
+		students << {:name => name.capitalize, :cohort => :august}
+		print "Now we have #{students.length} students\n"
 		# get another name from the user
 		name = gets.chomp
 	end
@@ -34,5 +33,18 @@ end
 
 students = input_students
 print_header
-print(students)
+print_student(students)
 print_footer(students)
+
+#Modify your program to only print the students whose name begins with any letter
+def student_search(studentlist)
+	puts "What starting letter would you like to search for:"
+	# get the search character
+	character = gets.chomp.capitalize
+	# perform search for character
+	studentlist.select do |student| 
+		puts "#{student[:name]}" if student[:name].start_with?(character)
+	end
+end
+
+student_search(students)
