dcc1871f486fcac9d00614f45f498d2a1d5e0f90
michballard <michballard@gmail.com>
2014-08-07 13:14:22 +0100

    Refactored and pass variables as argurments

--- directory.rb
+++ directory.rb
@@ -1,5 +1,6 @@
 $cohort_months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 $default_cohort_month = "August"
+@students = []
 
 def input_student
 	# get the first student's details
@@ -41,18 +42,17 @@ def input_students
 	puts "Please enter the details of each student"
 	puts "To finish, just press return"
 	# create an empty array
-	student_input = []
 	loop do
 		student = input_student
 		break if student == nil
-		student_input << student
-		print "Now we have #{student_input.length} student"
-		if student_input.length != 1
+		@students << student
+		print "Now we have #{@students.length} student"
+		if @students.length != 1
 			print "s"
 		end
 		print "\n"
 	end	
-	return student_input
+	return @students
 end
 
 def print_header
@@ -79,74 +79,101 @@ end
 # end
 
 # Rewriting above code using "until" control flow method
-def print_student(studentlist)
+def print_students_list
 	count = 0
-	until count >= studentlist.length
-		student = studentlist[count]
+	until count >= @students.length
+		student = @students[count]
 		count += 1			
 		puts "#{count}. #{student[:name]} (#{student[:cohort]} cohort)"
 		puts "   Hobbies: #{student[:hobbies]}\n   Birth country: #{student[:cob]}\n   Height: #{student[:height]}"
 	end
 end
 
-def print_footer(names)
-	print "Overall, we have #{names.length} great student"
-	if names.length != 1
+def print_footer
+	print "Overall, we have #{@students.length} great student"
+	if @students.length != 1
 	  	print "s"
 	end
   	print "\n"
 end
 
 # Lists students only if there is 1 or more students saved in array
-def print_student_list(studentlist)
-	if studentlist.length <= 0
-		puts "No students found in student list - End of program"
+def show_students
+	if @students.length <= 0
+		puts "No students found in student list"
 	else 
 		print_header
-		print_student(studentlist)
-		print_footer(studentlist)
+		print_students_list
+		print_footer
 	end
 end
 
 # List only students whose name begins with a specified letter
-def student_search(studentlist)
+def student_search
 	puts "What is the starting letter you would like to search for?"
 	# get the search character from user
 	character = gets.chomp.capitalize
 	puts "Names starting with #{character}:"
 	# perform search for character
-	studentlist.select do |student| 
+	@students.select do |student| 
 		puts "#{student[:name]}" if student[:name].start_with?(character)
 	end
 end
 
 # List only students with names shorter than specified number of characters
-def students_name_length(studentlist)
+def students_name_length
 	puts "What is the required maximum number of characters in full name?"
 	# get number of characters from user
 	namelength = gets.chomp.to_i
 	puts "Names with up to #{namelength} characters:"
 	# perform search for name lengths
-	studentlist.select do |student|
+	@students.select do |student|
 		puts "#{student[:name]}" if student[:name].length <= namelength
 	end
 end	
 
-def cohorts(studentlist)
-	cohortlist = (studentlist.map { |student| student[:cohort] }).uniq
+# Lists students grouped by cohort
+def cohorts
+	cohortlist = (@students.map { |student| student[:cohort] }).uniq
 	cohortlist.each do |cohort|
 		puts "#{cohort} cohort:"
-		studentlist.select do |student|
+		@students.select do |student|
 			puts "#{student[:name]}" if student[:cohort] == cohort
 		end
 	end
 end
 
-students = input_students
+def print_menu
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "9. Exit" 
+end
 
-print_student_list (students)
+def process(selection)
+	case selection
+		when "1"
+			# input the students
+			@students = input_students
+		when "2"
+			# show the students
+			show_students
+		when "9"
+			exit #this will cause the program to terminate
+		else
+			puts "I don't know what you meant, try again"
+	end 
+end
 
-student_search(students)
-students_name_length(students)
+def interactive_menu
+	loop do
+		print_menu
+		process(gets.chomp)
+	end
+end
 
-cohorts(students)
+interactive_menu
+
+# student_search(@students)
+# students_name_length(@students)
+# cohorts(@students)
+
